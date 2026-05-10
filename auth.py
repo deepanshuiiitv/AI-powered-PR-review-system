78ae9db7bbc9dc714feb6edfc96a315676c507c8
Deepanshu Verma <djpubg1026@gmail.com>
2026-05-10 17:55:04 +0530

    auth updated

--- auth.py
+++ auth.py
@@ -4,3 +4,7 @@ def login(username):
     query = f"SELECT * FROM users WHERE name = '{username}'"
     print("Logging in user...")
     return query
+def register(username, password):
+    query = f"INSERT INTO users (name, password) VALUES ('{username}', '{password}')"
+    print("Registering user...")
+    return query
